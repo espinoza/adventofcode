14def392ce411f1f57f8952b29b5724344612ad6
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2022-12-02 01:35:09 -0300

    2022 Day 01 problem 1

---
+++ 2022/day01/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = File.ReadAllText("input.txt");
+string[] elvesInventories = input.Split(new string[]{"\n\n"}, StringSplitOptions.None);
+int maxCalories = elvesInventories.Max(GetTotalCalories);
+System.Console.WriteLine(maxCalories);
+
+static int GetTotalCalories(string elfInventory)
+{
+    int[] calories = elfInventory.Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    return calories.Aggregate(0, (cal1, cal2) => cal1 + cal2);
+}
