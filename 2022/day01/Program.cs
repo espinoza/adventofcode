237e575440e170a813d041bb6421aafc9d64fb4c
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2022-12-08 23:44:14 -0300

    2022 Day 01 problem 2

--- 2022/day01/Program.cs
+++ 2022/day01/Program.cs
@@ -1,10 +1,18 @@
-﻿string input = File.ReadAllText("input.txt");
-string[] elvesInventories = input.Split(new string[]{"\n\n"}, StringSplitOptions.None);
-int maxCalories = elvesInventories.Max(GetTotalCalories);
-System.Console.WriteLine(maxCalories);
+﻿string text = File.ReadAllText("input.txt");
+Console.WriteLine("Advent of Code 2022 - Day 01");
+Console.WriteLine($"Part 1: {GetTopCalories(text, 1).First()}");
+Console.WriteLine($"Part 2: {GetTopCalories(text, 3).Sum()}");
 
-static int GetTotalCalories(string elfInventory)
+static int[] GetTopCalories(string text, int numberOfTopElves)
 {
-    int[] calories = elfInventory.Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-    return calories.Aggregate(0, (cal1, cal2) => cal1 + cal2);
+    string[] inventories = text.Trim().Split("\n\n");
+    int[] topCalories = new int[numberOfTopElves];
+    foreach (string inventory in inventories)
+    {
+        int currentCalories = inventory.Split('\n').Select(int.Parse).Sum();
+        int minCaloriesInTop = topCalories.Min();
+        if (currentCalories > minCaloriesInTop)
+            topCalories[Array.IndexOf(topCalories, minCaloriesInTop)] = currentCalories;
+    }
+    return topCalories;
 }
