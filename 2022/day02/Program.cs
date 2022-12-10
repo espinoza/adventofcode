56fee4c861ef7fde5cd355c3231a14267d73f362
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2022-12-10 01:44:08 -0300

    2022 Day 02 problem 2

--- 2022/day02/Program.cs
+++ 2022/day02/Program.cs
@@ -1,26 +1,65 @@
 ﻿string text = File.ReadAllText("input.txt").Trim();
 Console.WriteLine("Advent of Code 2022 - Day 02");
 
+var shapeScores = new Dictionary<string, int> { {"A", 1}, {"B", 2}, {"C", 3} };
 var elfChoices = new string[3] {"A", "B", "C"};
 var ownChoices = new string[3] {"X", "Y", "Z"};
-var decrypted = new Dictionary<string, string>
+
+var choicesOpponents = new Dictionary<string, OpponentRole>
 {
-    ["A"]="r", ["B"]="p", ["C"]="s", ["X"]="r", ["Y"]="p", ["Z"]="s"
+    { "A", new OpponentRole {Loser="C", Tie="A", Winner="B"} },
+    { "B", new OpponentRole {Loser="A", Tie="B", Winner="C"} },
+    { "C", new OpponentRole {Loser="B", Tie="C", Winner="A"} },
 };
-var loser = new Dictionary<string, string> { {"r", "s"}, {"s", "p"}, {"p", "r"} };
-var shapeScore = new Dictionary<string, int> { {"r", 1}, {"p", 2}, {"s", 3} };
-var roundScore = new Dictionary<string, int>();
+
+
+// Part 1
+
+var roundScores1 = new Dictionary<string, int>();
+var decryptedChoice = new Dictionary<string, string> { ["X"]="A", ["Y"]="B", ["Z"]="C" };
 
 foreach (string elfChoice in elfChoices)
 {
     foreach (string ownChoice in ownChoices)
     {
-        bool draw = decrypted[ownChoice] == decrypted[elfChoice];
-        bool win = loser[decrypted[ownChoice]] == decrypted[elfChoice];
-        int scoreByOutcome = (draw ? 3 : 0) + (win ? 6 : 0);
-        roundScore[$"{elfChoice} {ownChoice}"] = scoreByOutcome + shapeScore[decrypted[ownChoice]];
+        bool tie = choicesOpponents[decryptedChoice[ownChoice]].Tie == elfChoice;
+        bool win = choicesOpponents[decryptedChoice[ownChoice]].Loser == elfChoice;
+        int scoreByOutcome = (tie ? 3 : 0) + (win ? 6 : 0);
+        roundScores1[$"{elfChoice} {ownChoice}"] = scoreByOutcome + shapeScores[decryptedChoice[ownChoice]];
     }
 }
 
-int score = text.Split('\n').Select(s => roundScore[s]).Sum();
+int score = text.Split('\n').Select(s => roundScores1[s]).Sum();
 System.Console.WriteLine($"Part 1: {score}");
+
+
+// Part 2
+
+var roundScores2 = new Dictionary<string, int>();
+var decryptedOwnRole = new Dictionary<string, Func<OpponentRole, string>>
+{
+    ["X"] = (x=>x.Loser), ["Y"] = (x=>x.Tie), ["Z"] = (x=>x.Winner)
+};
+
+foreach (string elfChoice in elfChoices)
+{
+    foreach (string ownChoice in ownChoices)
+    {
+        bool tie = ownChoice == "Y";
+        bool win = ownChoice == "Z";
+        int scoreByOutcome = (tie ? 3 : 0) + (win ? 6 : 0);
+        string ownShape = decryptedOwnRole[ownChoice](choicesOpponents[elfChoice]);
+        roundScores2[$"{elfChoice} {ownChoice}"] = scoreByOutcome + shapeScores[ownShape];
+    }
+}
+
+score = text.Split('\n').Select(s => roundScores2[s]).Sum();
+System.Console.WriteLine($"Part 2: {score}");
+
+
+class OpponentRole
+{
+    public string? Loser {get; set;}
+    public string? Tie {get; set;}
+    public string? Winner {get; set;}
+}
